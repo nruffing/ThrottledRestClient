3634f2965b85133339ab810772a5b5fd72eb88da
Nicholas Ruffing <nicholasruffing70@gmail.com>
2021-01-06 18:01:06 -0500

    Initial Commit

---
+++ ConstrainedQueue.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Validation;
+
+namespace ThrottledRestClient
+{
+    internal sealed class ConstrainedQueue<T>
+    {
+        private readonly int _capacity;
+        private readonly Queue<T> _queue;
+
+        internal ConstrainedQueue(int capacity)
+        {
+            Requires.Range(capacity > 0, nameof(capacity));
+
+            this._capacity = capacity;
+            this._queue = new Queue<T>();
+        }
+
+        internal bool IsFull => this._queue.Count == this._capacity;
+
+        internal void Enqueue(T item)
+        {
+            this._queue.Enqueue(item);
+            if (this._queue.Count > this._capacity)
+            {
+                this._queue.Dequeue();
+            }
+        }
+
+        internal T Peek()
+        {
+            return this._queue.Peek();
+        }
+    }
+}
---
+++ ThrottledRestClient.cs
@@ -0,0 +1,50 @@
+﻿using RestSharp;
+using System;
+using System.Threading.Tasks;
+using Validation;
+
+namespace ThrottledRestClient
+{
+    public sealed class ThrottledRestClient
+    {
+        private readonly IRestClient _client;
+        private readonly ConstrainedQueue<long> _timestampQueue;
+        private readonly int _throttlePeriodMilliseconds;
+        private readonly int _throttleRequestLimit;
+
+        public ThrottledRestClient(string baseUrl, int throttlePeriodMilliseconds, int throttleRequestLimit)
+        {
+            Requires.NotNullOrWhiteSpace(baseUrl, nameof(baseUrl));
+            Requires.Range(throttlePeriodMilliseconds > 0, nameof(throttlePeriodMilliseconds));
+            Requires.Range(throttleRequestLimit > 0, nameof(throttleRequestLimit));
+
+            this._throttlePeriodMilliseconds = throttlePeriodMilliseconds;
+            this._throttleRequestLimit = throttleRequestLimit;
+
+            this._client = new RestClient(baseUrl);
+            this._timestampQueue = new ConstrainedQueue<long>(this._throttleRequestLimit);
+        }
+
+        public async Task<IRestResponse<T>> ExecuteAsync<T>(IRestRequest request)
+        {
+            Requires.NotNull(request, nameof(request));
+
+            if (this._timestampQueue.IsFull)
+            {
+                var now = DateTime.Now.Ticks / 10000;
+                var peeked = this._timestampQueue.Peek() / 10000;
+
+                if (peeked > (now - this._throttlePeriodMilliseconds))
+                {
+                    await Task.Delay((int)(peeked - now + this._throttlePeriodMilliseconds)).ConfigureAwait(false);
+                }
+            }
+
+            this._timestampQueue.Enqueue(DateTime.Now.Ticks);
+            return await this._client.ExecuteAsync<T>(request).ConfigureAwait(false);
+        }
+
+        public void AddDefaultHeader(string name, string value)
+            => this._client.AddDefaultHeader(name, value);
+    }
+}
